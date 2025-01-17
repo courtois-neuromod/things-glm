79d11f1049a18e72f2780917b56110c1f9884dcb
Elizabeth DuPre <emd222@cornell.edu>
2025-01-17 15:59:29 -0500

    Update viz script to match new dir structure

--- code/visualize-memory-ffx.py
+++ code/visualize-memory-ffx.py
@@ -20,7 +20,7 @@ os.environ["PATH"] += ":/Applications/Inkscape.app/Contents/MacOS/"
 @click.option("--data_dir", default="/Users/emdupre/Desktop", help="Data directory.")
 def main(sub_name, stat_type, data_dir):
     """ """
-    sub_names = ["sub-01", "sub-02", "sub-03"]
+    sub_names = ["sub-01", "sub-02", "sub-03", "sub-06"]
     if sub_name not in sub_names:
         warn_msg = "Unrecognized subject {sub_name}"
         raise UserWarning(warn_msg)
@@ -30,7 +30,7 @@ def main(sub_name, stat_type, data_dir):
         warn_msg = "Unrecognized stat type {stat_type}"
         raise UserWarning(warn_msg)
 
-    beta_fname = f"{sub_name}_task-things_space-T1w_model-fitHrfGLMdenoiseRR_stats-trialBetas_desc-zscore_statseries.h5"
+    beta_fname = f"{sub_name}_task-things_space-T1w_model-fitHrfGLMdenoiseRR_stats-imageBetas_desc-zscore_statseries.h5"
     beta_h5 = h5py.File(Path(data_dir, "things-encode", "betas", beta_fname), "r")
     mask = nib.nifti1.Nifti1Image(
         np.array(beta_h5["mask_array"]), affine=np.array(beta_h5["mask_affine"])
@@ -38,7 +38,7 @@ def main(sub_name, stat_type, data_dir):
 
     ffx_fname = f"{sub_name}_task-things_space-T1w_contrast-HitWithinvCorrectRej_stat-{stat_type}_statmap.nii.gz"
     try:
-        ffx_nii = nib.load(Path(data_dir, "things-glm", ffx_fname))
+        ffx_nii = nib.load(Path(data_dir, "things-glm", sub_name, "glm", ffx_fname))
     except FileNotFoundError:
         warn_msg = (
             f"Statmap not found for subject {sub_name}. "
@@ -70,21 +70,10 @@ def main(sub_name, stat_type, data_dir):
         subject=sub_name,
         xfmname="align_auto",
         mask=mask.get_fdata(),
-        vmin=(np.max(ffx_nii.dataobj) * 0.05),
-        vmax=(np.max(ffx_nii.dataobj) * 0.95),
-        cmap="magma",
+        # vmin=(np.min(ffx_nii.dataobj) * 0.95),
+        # vmax=(np.max(ffx_nii.dataobj) * 0.95),
+        # cmap="magma",
     )
-    # _ = cortex.quickflat.make_figure(
-    #     ffx_vol,
-    #     with_colorbar=True,
-    #     colorbar_location="left",
-    #     with_curvature=True,
-    #     sampler="trilinear",
-    #     with_labels=False,
-    #     with_rois=True,
-    #     curv_brightness=1.0,
-    #     dpi=300,
-    # )
     cortex.quickflat.make_png(
         f"{sub_name}_task-things_space-T1w_contrast-HitWithinvCorrectRej_stat-{stat_type}_flatmap.png",
         ffx_vol,
